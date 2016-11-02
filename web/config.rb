11398c11261fb7a4a1e3e226d68dfbd7cc300c54
Zhengquan Bai <zhengquanbai@qf-huilongl-mac.local>
2016-11-02 11:17:15 +0800

    在config.rb中添加自定义sass mixin路径

--- web/config.rb
+++ web/config.rb
@@ -3,10 +3,11 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
+css_dir = "./"
+sass_dir = "./"
 images_dir = "images"
 javascripts_dir = "javascripts"
+add_import_path __dir__ + "/sass-mixins"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
