3da5a9e9f91910c284b2d2dc6c01800043fc1d6a
Zhengquan Bai <zhengquanbai@qf-huilongl-mac.local>
2016-11-03 16:22:30 +0800

    去除css/styles.scss

--- web/src/app.js
+++ web/src/app.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import Game from 'Components/Game';
 
-import '../resources/css/styles';
 import 'include_in_entry';
 
 render(<Game />, document.getElementById('anchor'));
