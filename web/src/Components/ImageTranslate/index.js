6ecd1d03fb0b67b9538f82ddf41e7038dd75ee35
jerryqueue <2117191088@qq.com>
2016-11-16 21:19:33 +0800

    skeleton of ImageTranslate

---
+++ web/src/Components/ImageTranslate/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vimer on 16/11/2016.
+ */
+import React from 'react';
+import image1 from ''
+import './image-translate.scss';
+
+class ImageTranslate extends React.Component {
+  render() {
+    return (
+        <div class="image-container">
+          <img src={} alt=""/>
+        </div>
+    );
+  }
+}
+
+export default ImageTranslate;
