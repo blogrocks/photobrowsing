7a11b4b37e1b0d62cf2e8c02bc791a704ae769aa
Zhengquan Bai <zhengquanbai@qf-huilongl-mac.local>
2016-11-23 18:18:15 +0800

    style

--- web/src/Components/ImageTranslateContainer/index.js
+++ web/src/Components/ImageTranslateContainer/index.js
@@ -14,16 +14,27 @@ var images = [image1, image2, image3, image4];
 class ImageTranslateContainer extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            galleries: [
+                <ImageTranslate imageSources={images} key="initial"/>
+            ]
+        };
     }
 
+    createGallery() {
+        let galleries = [...this.state.galleries];
+        galleries.push(<ImageTranslate photoAddingAllowed key={galleries.length} />);
+        this.setState({galleries});
+    }
     render() {
         return (
             <div class="container">
-                <a class="newgallary">
+                <a class="newgallary" onClick={() => this.createGallery()}>
                     <span>点此创建新影集</span>
                 </a>
-                <ImageTranslate imageSources={images}/>
-                <ImageTranslate photoAddingAllowed />
+                <div class="galleryContainer">
+                    {this.state.galleries}
+                </div>
             </div>
         );
     }
