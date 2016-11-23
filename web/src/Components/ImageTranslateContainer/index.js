223897ec648e04ef8b044863017f5f216c0326b4
jerryqueue <2117191088@qq.com>
2016-11-23 23:36:32 +0800

    finish

--- web/src/Components/ImageTranslateContainer/index.js
+++ web/src/Components/ImageTranslateContainer/index.js
@@ -3,14 +3,14 @@
  */
 import React from 'react';
 import ImageTranslate from 'Components/ImageTranslate';
-import image1 from './images/1.jpg';
-import image2 from './images/2.jpg';
-import image3 from './images/3.jpg';
-import image4 from './images/4.jpg';
+import image1 from './images/boy1.jpg';
+import image2 from './images/boy2.jpg';
+import image3 from './images/boy3.jpg';
+import image4 from './images/boy4.jpg';
 
 import './image_container.scss';
 
-var images = [image1, image2, image3, image4];
+var images = [image1, image2, image3];
 class ImageTranslateContainer extends React.Component {
     constructor(props) {
         super(props);
@@ -26,15 +26,35 @@ class ImageTranslateContainer extends React.Component {
         galleries.push(<ImageTranslate photoAddingAllowed key={galleries.length} />);
         this.setState({galleries});
     }
+
+    calcRows(galleries) {
+        let rows = [];
+
+        for(let i = 0; i < galleries.length; i += 2) {
+            if ((i + 1) < galleries.length) {
+                rows.push(
+                    <div class="galleryContainer" key={i}>
+                        {galleries[i]}
+                        {galleries[i+1]}
+                    </div>
+                );
+            } else {
+                rows.push(
+                    <div class="galleryContainer" key={i}>
+                        {galleries[i]}
+                    </div>
+                );
+            }
+        }
+        return rows;
+    }
     render() {
         return (
             <div class="container">
                 <a class="newgallary" onClick={() => this.createGallery()}>
                     <span>点此创建新影集</span>
                 </a>
-                <div class="galleryContainer">
-                    {this.state.galleries}
-                </div>
+                {this.calcRows(this.state.galleries)}
             </div>
         );
     }
